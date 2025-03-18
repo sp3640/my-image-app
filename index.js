8f578e67b74278c27ae19f12410fa86be4c70805
SIDDHARTH RAI <104321847+sp3640@users.noreply.github.com>
2025-03-18 08:56:01 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import UploadForm from '../components/UploadForm';
+
+export default function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <UploadForm />
+    </div>
+  );
+}
